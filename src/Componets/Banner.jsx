92312820620ad9d4fe778401f33dfc274ee9f5f9
Jesus Hernandez <hdmjesus@gmail.com>
2021-03-06 17:56:52 -0400

    defaultExtractor agregado para purgar correctamente el css de tailwind

--- src/Componets/Banner.jsx
+++ src/Componets/Banner.jsx
@@ -26,7 +26,7 @@ const Banner = () => {
       }}
       className='banner flex justify-center items-center h-72 sm:h-96'>
       <article className='banner__actions mt-10 flex flex-col min-w-200 w-1/2 max-w-sm items-center'>
-        <div className='banner__description bg-actions flex flex-col p-2.5 rounded-lg w-full items-center h-24 lg:mt-9 '>
+        <div className='banner__description bg-actions flex flex-col justify-center p-2.5 rounded-lg w-full items-center h-24 lg:mt-9 '>
           <h1 className='font-title text-5xl text-white'>Vps Doll</h1>
           <p className='font-body text-white font-light'>Sthetic Studio</p>
         </div>
